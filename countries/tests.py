abe3dab20a0b55a92c15ac3e11c21584f08700ec
sita1013 <seluvaiasariahita@gmail.com>
2025-04-13 17:11:58 +0100

    added a contriesconfig test

--- countries/tests.py
+++ countries/tests.py
@@ -1,18 +1,11 @@
 from django.test import TestCase
+from django.apps import apps
+from countries.apps import CountriesConfig
 
-# unit tests
+def test_app_config_name():
+    assert CountriesConfig.name == 'countries'
 
+def test_app_is_registered():
+    app_config = apps.get_app_config('countries')
+    assert isinstance(app_config, CountriesConfig)
 
-
-
-
-
-
-#end-to-end tests
-
-
-
-
-
-
-#smoke tests
